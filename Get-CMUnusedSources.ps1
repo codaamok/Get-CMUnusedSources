d66d7f66775bb2f7cc3ddcc2c427c6c2a6ba0130
Adam Cook <adam@cookuop.co.uk>
2019-05-24 22:04:22 +0100

    Update Get-CMUnusedSources.ps1
    Improved exception handling within Set-CMDrive

--- Get-CMUnusedSources.ps1
+++ Get-CMUnusedSources.ps1
@@ -357,12 +357,40 @@ Function Get-AllSharedFolders {
     return $AllShares
 }
 Function Set-CMDrive {
-    Import-Module $env:SMS_ADMIN_UI_PATH\..\ConfigurationManager.psd1
-    #If the PS drive doesn't exist then try to create it.
-    If (! (Test-Path "$($SiteCode):")) {
-        New-PSDrive -Name $SiteCode -PSProvider CMSite -Root "." | Out-Null
+    Param(
+        [string]$SiteCode,
+        [string]$Server,
+        [string]$Path
+    )
+
+    # Import the ConfigurationManager.psd1 module 
+    if((Get-Module ConfigurationManager) -eq $null) {
+        try {
+            Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1"
+        } catch {
+            Throw "Failed to import Configuration Manager module"
+        }
     }
-    Set-Location "$($SiteCode):" | Out-Null
+
+    try {
+        # Connect to the site's drive if it is not already present
+        If((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+            New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $Server -ErrorAction Stop | Out-Null
+        }
+        # Set the current location to be the site code.
+        Set-Location "$($SiteCode):\" -ErrorAction Stop
+
+        # Verify given sitecode
+        If((Get-CMSite -SiteCode $SiteCode).SiteCode -ne $SiteCode) { throw }
+
+    } catch {
+        If((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -ne $null) {
+            Set-Location $Path
+            Remove-PSDrive -Name $SiteCode -Force
+        }
+        Throw "Failed to create New-PSDrive with site code `"$($SiteCode)`" and server `"$($Server)`""
+    }
+
 }
 
 [System.Collections.ArrayList]$AllContentObjects = @()
@@ -408,7 +436,7 @@ $SCCMServer = $FQDN.Split(".")[0]
 If ($SourcesLocation -match "^[a-zA-Z]:$") { $SourcesLocation = $SourcesLocation + "\" }
 
 $OriginalPath = (Get-Location).Path
-Set-CMDrive
+Set-CMDrive -SiteCode $SiteCode -Server $SCCMServer -Path $OriginalPath
 
 #Write-Debug "Getting content"
 Write-Progress -Id 1 -Activity "Running Get-CMUnusedSources" -PercentComplete 0 -Status "Getting all CM content objects"
